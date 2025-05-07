ced1c93f61b6b4782a1e6d163bcbb91bcb1b061f
romain.thinon@laposte.net <romain.thinon@laposte.net>
2025-05-07 09:48:22 +0000

    mart table

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -5,6 +5,9 @@ SELECT
   order_margin.quantity, 
   order_margin.purchase_cost, 
   order_margin.margin, 
+  ship.shipping_fee, 
+  ship.logcost,
+  ship.ship_cost,
   ROUND(SUM(order_margin.margin + ship.shipping_fee - ship.logcost - ship.ship_cost), 2) AS op_margin
 FROM {{ ref('int_orders_margin') }} AS order_margin
 JOIN {{ ref('stg_gz_raw_data__raw_gz_ship') }} AS ship
@@ -15,4 +18,7 @@ GROUP BY
   order_margin.revenue, 
   order_margin.quantity, 
   order_margin.purchase_cost,
-  order_margin.margin
+  order_margin.margin,
+  ship.shipping_fee, 
+  ship.logcost,
+  ship.ship_cost
---
+++ models/mart/finance_days.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='table') }}
+
+
+SELECT date_date,
+ROUND(SUM(revenue), 2) as daily_revenue, 
+ROUND(SUM(ship_cost), 2) as daily_ship_cost, 
+ROUND(SUM(op_margin), 2) as daily_op_margin, 
+ROUND(SUM(purchase_cost), 2) as daily_purchase_cost, 
+ROUND(SUM(shipping_fee), 2) as daily_shipping_fee,
+ROUND(SUM(logcost), 2) as daily_logcost,
+COUNT(DISTINCT orders_id) as daily_transaction, 
+ROUND(SUM(revenue)/COUNT(DISTINCT orders_id), 2) as avg_basket
+FROM {{ ref('int_orders_operational') }} 
+GROUP BY date_date
+ORDER BY date_date DESC 
+
